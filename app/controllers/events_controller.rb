75724a701721837ade0166f791e5275c29e9fed9
Candela Diaz <candeladiazzz.an@gmail.com>
2021-04-13 15:10:18 -0300

    views:event-edit

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,27 +1,34 @@
 class EventsController < ApplicationController
+    
+    #GET events/new
     def new
         @event = Event.new
     end
 
+    #GET events/:id
     def show
         @event = Event.find(params[:id])
     end
 
+    #GET events/:id/edit
     def edit
         @event = Event.find(params[:id])
         puts
     end
 
+    #PATCH events/:id
     def update
         @event = Event.update(title: params[:event][:title], description: params[:event][:description])
         
         redirect_to @event
     end
 
-    
+    #POST /events
     def create
-        @event = Event.create(title: params[:event][:title], description: params[:event][:description])
-    #   redirect_to @event
+        @event = Event.new(title: params[:event][:title],
+                              description: params[:event][:description]) 
+        
+        render json: @event
      end
 
 end
